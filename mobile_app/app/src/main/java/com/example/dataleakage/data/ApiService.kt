c6ebc27f4431e1f3642b9a5b3e0a013db66c3a62
hrdhruv <harshdhruv889@gmail.com>
2026-01-15 18:08:22 +0530

    initial view

--- mobile_app/app/src/main/java/com/example/dataleakage/data/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dataleakage
-
-interface AnalysisApi {
-    @POST("/analyze")
-    suspend fun getAnalysis(@Body request: ScanRequest): AnalysisResponse
-}
-
-data class ScanRequest(val package_name: String, val permissions: List<String>)
-data class AnalysisResponse(val risk_level: String, val score: Int, val flags: List<String>)
